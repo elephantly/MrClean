d98095eedadcf24f9c48f76cd63228c9722e5e22
PurpleBabar <lalung.alexandre@gmail.com>
2017-03-23 18:32:46 +0100

    adding first shot

---
+++ src/Command.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace MrClean;
+
+/**
+ *
+ */
+class Command
+{
+
+    protected $_flags;
+
+    public function __construct(array $arguments)
+    {
+        $this->_flags = array();
+        foreach ($arguments as $value)
+        {
+            if (preg_match("/--\w+/", $value))
+            {
+                $this->_flags[] = $value;
+            }
+        }
+    }
+
+    public function getFlags()
+    {
+        return $this->_flags;
+    }
+
+    public function hasFlags()
+    {
+        return count($this->_flags);
+    }
+
+    public function is($term)
+    {
+        if (in_array("--".$term, $this->_flags))
+        {
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ src/Terminal.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace MrClean;
+
+/**
+ *
+ */
+class Terminal
+{
+    private $success="\033[0;32m";
+    private $error="\033[0;31m";
+    private $nc="\033[0m"; # No Color
+
+    public function write($text, $line = true)
+    {
+        echo $line ? $text."\n" : $text;
+    }
+
+    public function error($text, $line = true)
+    {
+        $text = $this->error.$text.$this->nc;
+        echo $line ? $text."\n" : $text;
+    }
+
+    public function success($text, $line = true)
+    {
+        $text = $this->success.$text.$this->nc;
+        echo $line ? $text."\n" : $text;
+    }
+}
